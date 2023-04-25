ebd22e3ae1238551c1eb2d658b9b52ffa1530215
powerbox1000 <72870790+powerbox1000@users.noreply.github.com>
2023-04-25 09:00:29 -0400

    Create AmongUs.lua

---
+++ scripts/AmongUs.lua
@@ -0,0 +1,41 @@
+---====== Define spawner ======---
+
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/Source.lua"))()
+
+---====== Create entity ======---
+
+local entity = Spawner.createEntity({
+    CustomName = "Among Us Impostor",
+    Model = "https://github.com/powerbox1000/doors-entity-scripts/blob/main/models/AmongUs.rbxm?raw=true",
+    Speed = 150,
+    MoveDelay = 2,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    SpawnInFront = false,
+    ShatterLights = true,
+    FlickerLights = {
+        Enabled = true,
+        Duration = 1
+    },
+    Cycles = {
+        Min = 1,
+        Max = 1,
+        Delay = 2
+    },
+    CamShake = {
+        Enabled = true,
+        Values = {1.5, 20, 0.1, 1},
+        Range = 100
+    },
+    ResistCrucifix = false,
+    BreakCrucifix = true,
+    DeathMessage = {
+      "You died to who you call the Among Us Impostor",
+    },
+    IsCuriousLight = false
+})
+
+---====== Run entity ======---
+
+Spawner.runEntity(entity)
